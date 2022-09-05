14776d7295110171153da175a3f7722480f2963d
MarlonVB <104545159+MarlonVB@users.noreply.github.com>
2022-09-05 00:23:20 -0500

    Diseño NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,41 +7,16 @@ import React from "react";
 const NavBar = () => {
     return (
         <>
-            {/*<div className="header-nav">*/}
-            {/*    <nav className="nav-menu">*/}
-            {/*        <ul className="nav-list-items">*/}
-            {/*            <li>*/}
-            {/*                <a href="/">Inicio</a>*/}
-            {/*            </li>*/}
-            {/*            <li>*/}
-            {/*                <a href="/estudiantes">Estudiantes</a>*/}
-            {/*            </li>*/}
-            {/*            <li>*/}
-            {/*                <a href="#">Docentes</a>*/}
-            {/*            </li>*/}
-            {/*            <li>*/}
-            {/*                <Link to="/materia">Asignaturas</Link>*/}
-            {/*            </li>*/}
-            {/*        </ul>*/}
-            {/*    </nav>*/}
-            {/*    <div>*/}
-
-            {/*    </div>*/}
-            {/*    <img className="logo" src="https://i.postimg.cc/65k595dJ/icons8-mortarboard-100px.png"/>*/}
-            {/*    <h1 className="title">Academico</h1>*/}
-            {/*</div>*/}
-
             <div>
-                <nav className="navbar navbar-expand-lg bg-darck">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Navbar</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+                        <a className="navbar-brand" href="/">
+                            <img src="https://i.postimg.cc/65k595dJ/icons8-mortarboard-100px.png" alt="" width="60"
+                                 className="d-inline-block mx-2"/>
+                                Academico
+                        </a>
+                        <div className="nav justify-content-center" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
                                     <a className="nav-link active" aria-current="page" href="/">Inicio</a>
                                 </li>
